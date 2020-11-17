42b80eff16aa3a1f1bdc6223abb42cd28908eafc
美濃越晨 <>
2020-11-17 22:10:04 +0900

    staticpageの作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | ワンマップ"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "お問い合わせ | ワンマップ"
   end
 
 end
