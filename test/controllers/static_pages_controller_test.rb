3adf2ccf32164347b9621056b5698b2dc21dbdc7
美濃越晨 <>
2020-11-18 15:40:48 +0900

    full_title helperの追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | ワンマップ"
+    assert_select "title","ワンマップ"
   end
 
   test "should get help" do
