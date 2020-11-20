7ab910b10d9123cfb04c161fe282cdf091ef66f7
美濃越晨 <>
2020-11-20 15:32:34 +0900

    restaurant一覧ビューの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/restaurants/index', to: 'restaurants#index'
 
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
