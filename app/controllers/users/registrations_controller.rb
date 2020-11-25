44ab99695d7744ffae98c2ba7dfa47bfe66a56f3
美濃越晨 <>
2020-11-25 20:31:14 +0900

    showビューの編集

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   # プロフィール画面用のアクションを追加
   def detail
     @user = User.find_by(id: params[:id])
   end
 
-  # protected
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # ここのコメントアウトを外してリダイレクト先を指定
   # ルートパス名でも良い
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/restaurants/index', to: 'restaurants#index'
+  get '/show', to:'users/registrations#show'
 
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
@@ -11,8 +13,8 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
+    get "/users/sign_up", :to => "users/registrations#new"
+    get "/users/sign_in", :to => "users/sessions#new"
+    get "/users/sign_out", :to => "users/sessions#destroy"
   end
 end
